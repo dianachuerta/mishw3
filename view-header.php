1f1ea44b8af1cc54493663923b18faf0a90b77ee
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-11-28 17:13:10 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,16 +20,13 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="post.php">Post</a>
+          <a class="nav-link" href="doctors.php">Doctors</a>
         </li>
          <li class="nav-item">
-          <a class="nav-link" href="post-result.php">Post result</a>
+          <a class="nav-link" href="patients.php">Patients</a>
         </li>
          <li class="nav-item">
-          <a class="nav-link" href="get.php">Get</a>
-        </li>
-         <li class="nav-item">
-          <a class="nav-link" href="get-result.php">Get result</a>
+          <a class="nav-link" href="doctors-with-patients.php">Doctors with patients</a>
         </li>
       </ul>
     </div>
