c47dda00548f9e5609b3fec0fefca5ba1323a24e
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-02 15:47:58 -0600

    Update patients.php

--- patients.php
+++ patients.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-doctors.php");
 
+$pageTitle = "Patients";
+include "view-header.php";
+$patients=selectPatients();
+include "view-patients.php";
+include "view-footer.php";
+?>
