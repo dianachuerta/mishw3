e78309e952c38959c9d68c2435ef6208ee608794
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-02 15:52:08 -0600

    Update model-patients.php

--- model-patients.php
+++ model-patients.php
@@ -1,1 +1,16 @@
+<?php
 
+function selectClients() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT doctor_id, doctor_name, specialization FROM `doctor`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
