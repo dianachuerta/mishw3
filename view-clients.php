5179791047790d509b138d202d514790eba9e85d
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-02 16:01:30 -0600

    Update view-clients.php

--- view-clients.php
+++ view-clients.php
@@ -1,1 +1,28 @@
+<h1>Clients</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Address</th>
+      </tr>
+    </thead>
+    <tbody>
 
+<?php
+
+while ($client = $clients->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $client ['client_id'];?></td>
+    <td><?php echo $client ['client_name'];?></td>
+    <td><?php echo $client ['client_address'];?></td>
+  </tr>
+<?php
+}
+?>
+   
+    </tbody>
+  </table>
+</div>
