7dc6f7f1415de2db606ce933c8c75de7240e9245
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-02 20:19:21 -0600

    Create model-doctors-with-clients.php

---
+++ model-doctors-with-clients.php
@@ -0,0 +1,16 @@
+<?php
+
+function selectDoctors() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT doctor_id, doctor_name, specialization FROM `doctor`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
