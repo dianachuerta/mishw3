d5066acffc09a272fb829e23cfeb7ff6c658c304
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-02 19:25:52 -0600

    Update consultations-by-course.php

--- consultations-by-course.php
+++ consultations-by-course.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-consultations-by-client.php");
 
+$pageTitle = "Consultations by client";
+include "view-header.php";
+$consultations=selectConsultationsByClient($_POST['cid']);
+include "view-consultations-by-client.php";
+include "view-footer.php";
+?>
