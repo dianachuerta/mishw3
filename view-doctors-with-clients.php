03f7bcfac6f49fad2c727d0a521a3ad4b100d08a
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-03 11:13:05 -0600

    Update view-doctors-with-clients.php

--- view-doctors-with-clients.php
+++ view-doctors-with-clients.php
@@ -12,7 +12,7 @@ while ($doctor = $doctors->fetch_assoc()) {
       <p class="card-text">
        <ul class="list-group">
 <?php
-  $clients= selectClientsByDoctor($doctor ['doctor_id'];
+  $clients= selectClientsByDoctor($doctor ['doctor_id']);
   while ($client= $clients ->fetch_assoc()) {
 ?>
    <li class="list-group-item"><?php echo $client ['client_name'];?> - <?php echo $client ['room'];?> - <?php echo $client ['day_time'];?> </li>
