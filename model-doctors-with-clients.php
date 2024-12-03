3023b432d93f086786d875ed6dcfe63f47504843
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-02 20:48:46 -0600

    Update model-doctors-with-clients.php

--- model-doctors-with-clients.php
+++ model-doctors-with-clients.php
@@ -13,4 +13,25 @@ function selectDoctors() {
         throw $e;
     }
 }
+
+function selectClientsByDoctor($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT c.client_id, client_name, client_address, room, day_time FROM `client` c JOIN consultation p on p.consultation_id = c.client_id WHERE p.doctor_id=?");
+      $stmt->bind_param("i", $iid );
+      $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+
+
 ?>
+
+
