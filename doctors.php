3f17df09b5675db973fc343bc05b04c1166b288e
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-03 20:55:02 -0600

    Update doctors.php

--- doctors.php
+++ doctors.php
@@ -3,8 +3,17 @@ require_once("util-db.php");
 require_once("model-doctors.php");
 
 $pageTitle = "Doctors";
-include "view-header.php";
-$doctors=selectDoctors();
-include "view-doctors.php";
-include "view-footer.php";
+include "view-header.php"; 
+$doctors = selectDoctors();
+
+?>
+<style>
+    body {
+        background-color: #ffebf2; 
+    }
+</style>
+
+<?php
+include "view-doctors.php";  
+include "view-footer.php";  
 ?>
