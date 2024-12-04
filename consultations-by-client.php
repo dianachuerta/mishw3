7e4b7fc77fb1d5d4e3a1bbec78709adf4e520c68
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-03 21:01:48 -0600

    Update consultations-by-client.php

--- consultations-by-client.php
+++ consultations-by-client.php
@@ -5,6 +5,17 @@ require_once("model-consultations-by-client.php");
 $pageTitle = "Consultations by client";
 include "view-header.php";
 $consultations=selectConsultationsByClient($_POST['cid']);
+
+?>
+<style>
+    body {
+        background-color: #ADD8E6; 
+    }
+</style>
+
+<?php
+
+
 include "view-consultations-by-client.php";
 include "view-footer.php";
 ?>
