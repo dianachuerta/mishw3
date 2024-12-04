a9e260500b9d27f949d7b322f48f785a2f971c70
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-03 20:27:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@ include "view-header.php";
 
 <style>
     h1 {
-        font-size: 48px; /* Big letters for "Final Project" */
-        background-color: lightblue; /* Light blue highlight */
-        padding: 10px 20px; /* Add padding around the text */
-        border-radius: 5px; /* Optional: rounded corners for the background */
-        display: inline-block; /* Makes the background only around the text */
+        font-size: 48px; 
+        background-color: lightblue; 
+        padding: 10px 20px; 
+        border-radius: 5px; 
+        display: inline-block; 
     }
 </style>
 
