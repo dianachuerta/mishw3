bc5204faf418f9a2cfb108b2ef3dcb325bcb5c80
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-03 21:03:57 -0600

    Update clients-by-doctor.php

--- clients-by-doctor.php
+++ clients-by-doctor.php
@@ -5,6 +5,14 @@ require_once("model-clients-by-doctor.php");
 $pageTitle = "Clients by Doctor";
 include "view-header.php";
 $clients=selectClientsByDoctor($_GET['id']);
+?>
+<style>
+    body {
+        background-color: #E6E6FA; 
+    }
+</style>
+
+<?php
 include "view-clients-by-doctor.php";
 include "view-footer.php";
 ?>
