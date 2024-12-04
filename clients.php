aeb88f42011417665b881e9e8082c8150733c7d1
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-03 20:57:44 -0600

    Update clients.php

--- clients.php
+++ clients.php
@@ -3,38 +3,44 @@ require_once("util-db.php");
 require_once("model-clients.php");
 
 $pageTitle = "Clients";
-include "view-header.php";
+include "view-header.php"; 
+?>
+<style>
+    
+    body {
+        background-color: #ffebf2; 
+    }
+</style>
 
+<?php
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add": 
-      if (insertClient($_POST['cName'],$_POST['cAddress'])) {
-      echo '<div class="alert alert-success" role="alert"> A client has been added!</div>';
+      if (insertClient($_POST['cName'], $_POST['cAddress'])) {
+        echo '<div class="alert alert-success" role="alert"> A client has been added!</div>';
       } else {
-      echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
+        echo '<div class="alert alert-danger" role="alert"> Error!</div>';
       }
       break;
 
-     case "Edit": 
-      if (updateClient($_POST['cName'],$_POST['cAddress'],$_POST['cid'])) {
-      echo '<div class="alert alert-success" role="alert"> A client has been edited!</div>';
+    case "Edit": 
+      if (updateClient($_POST['cName'], $_POST['cAddress'], $_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert"> A client has been edited!</div>';
       } else {
-      echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
+        echo '<div class="alert alert-danger" role="alert"> Error!</div>';
       }
       break;
-    
-      case "Delete": 
+
+    case "Delete": 
       if (deleteClient($_POST['cid'])) {
-      echo '<div class="alert alert-success" role="alert"> A client has been deleted!</div>';
+        echo '<div class="alert alert-success" role="alert"> A client has been deleted!</div>';
       } else {
-      echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
+        echo '<div class="alert alert-danger" role="alert"> Error!</div>';
       }
       break;
-   
   }
 }
-
-$clients=selectClients();
-include "view-clients.php";
-include "view-footer.php";
+$clients = selectClients();
+include "view-clients.php"; 
+include "view-footer.php";  
 ?>
