47c331e19fbdde351a5be762b677a4ddf3eed6bb
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-03 20:59:44 -0600

    Update doctors-with-clients.php

--- doctors-with-clients.php
+++ doctors-with-clients.php
@@ -5,6 +5,15 @@ require_once("model-doctors-with-clients.php");
 $pageTitle = "Doctors with Clients";
 include "view-header.php";
 $doctors=selectDoctors();
+?>
+<style>
+    body {
+        background-color: #ffebf2; 
+    }
+</style>
+
+<?php
+
 include "view-doctors-with-clients.php";
 include "view-footer.php";
 ?>
