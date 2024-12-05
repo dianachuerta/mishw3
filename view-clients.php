108f9280d86b2770e2b1c63a8f242f20d69b76dc
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-05 00:56:51 -0600

    Update view-clients.php

--- view-clients.php
+++ view-clients.php
@@ -1,14 +1,30 @@
-<div class= "row">
-  <div class="col" >
+<div class="row mb-3">
+  <div class="col">
     <h1>Clients</h1>
   </div>
   <div class="col-auto">
-
-    <?php
-  include "view-clients-newform.php";
-  ?>
-
-    
+    <?php include "view-clients-newform.php"; ?>
+  </div>
+  <div class="col-auto">
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="clientDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+        Filter by Client
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="clientDropdown">
+        <?php
+        $allClients = fetchAllClients(); // Replace with your function to get all clients
+        while ($client = $allClients->fetch_assoc()) {
+        ?>
+          <li>
+            <a class="dropdown-item" href="view-client-details.php?cid=<?php echo $client['client_id']; ?>">
+              <?php echo $client['client_name']; ?>
+            </a>
+          </li>
+        <?php
+        }
+        ?>
+      </ul>
+    </div>
   </div>
 </div>
 
@@ -20,49 +36,41 @@
         <th>Name</th>
         <th>Address</th>
         <th></th>
-         <th></th>
-         <th></th>
+        <th></th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
-
-<?php
-
-while ($client = $clients->fetch_assoc()) {
-?>
-  <tr>
-    <td><?php echo $client ['client_id'];?></td>
-    <td><?php echo $client ['client_name'];?></td>
-    <td><?php echo $client ['client_address'];?></td>
-    <td>
-       <?php
-  include "view-clients-editform.php";
-  ?>
-    </td>
-    <td>
-        <form method="post" action="">
-        <input type="hidden" name="cid" value="<?php echo $client ['client_id'];?>">
-          <input type="hidden" name="actionType" value="Delete">
-            <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+      <?php
+      while ($client = $clients->fetch_assoc()) {
+      ?>
+        <tr>
+          <td><?php echo $client['client_id']; ?></td>
+          <td><?php echo $client['client_name']; ?></td>
+          <td><?php echo $client['client_address']; ?></td>
+          <td><?php include "view-clients-editform.php"; ?></td>
+          <td>
+            <form method="post" action="">
+              <input type="hidden" name="cid" value="<?php echo $client['client_id']; ?>">
+              <input type="hidden" name="actionType" value="Delete">
+              <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-              </svg>
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                </svg>
               </button>
-          </form>
-    </td>
-      
-    <td>
-          <form method="post" action="consultations-by-client.php">
-            <input type="hidden" name="cid" value="<?php echo $client ['client_id'];?>">
-                <button type="submit" class="btn btn-primary">Consultations</button>
-              </form>
-    </td>
-  </tr>
-<?php
-}
-?>
-   
+            </form>
+          </td>
+          <td>
+            <form method="post" action="consultations-by-client.php">
+              <input type="hidden" name="cid" value="<?php echo $client['client_id']; ?>">
+              <button type="submit" class="btn btn-primary">Consultations</button>
+            </form>
+          </td>
+        </tr>
+      <?php
+      }
+      ?>
     </tbody>
   </table>
 </div>
