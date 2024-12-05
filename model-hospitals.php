65a0caa83ff822d728278de866b7aa3e85ac738f
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-04 23:26:47 -0600

    Update model-hospitals.php

--- model-hospitals.php
+++ model-hospitals.php
@@ -13,4 +13,49 @@ function selectHospitals() {
         throw $e;
     }
 }
+
+function insertHospitals ($hName, $hLocation) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `hospital` ( `hospital_name`, `hospital_location`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $hName, $hLocation);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateHospitals($hName, $hLocation, $hid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `hospital` set `hospital_name` = ? , `hospital_location` = ? where hospital_id = ?");
+        $stmt->bind_param("ssi", $hName, $hLocation, $hid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deleteClient($hid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from hospital where hospital_id = ?");
+        $stmt->bind_param("i", $hid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 ?>
