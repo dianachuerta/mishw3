fe6d5be1632187c6e2f5f5f68ec1c36ffa86bb71
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-05 00:34:43 -0600

    Update model-doctors.php

--- model-doctors.php
+++ model-doctors.php
@@ -13,4 +13,49 @@ function selectDoctors() {
         throw $e;
     }
 }
+
+function insertDoctor($dName, $dSpecialization) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `doctor` ( `doctor_name`, `specialization`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $dName, $dSpecialization);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateDoctor($dName, $dSpecialization, $did) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `doctor` set `doctor_name` = ? , `specialization` = ? where doctor_id = ?");
+        $stmt->bind_param("ssi", $dName, $dSpecialization, $did);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deleteDoctor($did) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from doctor where doctor_id = ?");
+        $stmt->bind_param("i", $did);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 ?>
