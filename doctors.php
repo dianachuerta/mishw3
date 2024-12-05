4ff787c802dcf6e1f6a0ad3b4b2bc22d6621d05c
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-05 13:03:25 -0600

    Update doctors.php

--- doctors.php
+++ doctors.php
@@ -3,8 +3,38 @@ require_once("util-db.php");
 require_once("model-doctors.php");
 
 $pageTitle = "Doctors";
-include "view-header.php"; 
-$doctors = selectDoctors();
+include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add": 
+      if (insertDoctor($_POST['dName'],$_POST['dSpecialization'])) {
+      echo '<div class="alert alert-success" role="alert"> A doctor has been added!</div>';
+      } else {
+      echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
+      }
+      break;
+
+     case "Edit": 
+      if (updateDoctor($_POST['dName'],$_POST['dSpecialization'],$_POST['did'])) {
+      echo '<div class="alert alert-success" role="alert"> A doctor has been edited!</div>';
+      } else {
+      echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
+      }
+      break;
+    
+      case "Delete": 
+      if (deleteDoctor($_POST['did'])) {
+      echo '<div class="alert alert-success" role="alert"> A doctor has been deleted!</div>';
+      } else {
+      echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
+      }
+      break;
+   
+  }
+}
+
+$doctors=selectDoctors();
 
 ?>
 <style>
@@ -14,6 +44,7 @@ $doctors = selectDoctors();
 </style>
 
 <?php
-include "view-doctors.php";  
-include "view-footer.php";  
+
+include "view-doctors.php";
+include "view-footer.php";
 ?>
