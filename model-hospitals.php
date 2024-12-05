4ef6bbde25aa2ee9fcdfe3ae1590c3f43e3403b0
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-05 13:44:27 -0600

    Create model-hospitals.php

---
+++ model-hospitals.php
@@ -0,0 +1,75 @@
+<?php
+
+function selectHospital() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT hospital_id, hospital_name, hospital_location FROM `hospital`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectHospitalForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT hospital_id, hospital_name FROM `hospital` order by hospital_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertHospital ($hName, $hLocation) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `hospital` ( `hospital_name`, `hospital_location`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $hName, $hLocation);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateHospital($hName, $hLocation, $hid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `hospital` set `hospital_name` = ? , `hospital_location` = ? where hospital_id = ?");
+        $stmt->bind_param("ssi", $hName, $hLocation, $hid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deleteHospital($hid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from hospital where hospital_id = ?");
+        $stmt->bind_param("i", $hid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+?>
