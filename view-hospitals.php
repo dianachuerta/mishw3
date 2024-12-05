857c8036f5e0bb38175be9ca5ad45b01c282ab29
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-04 23:54:52 -0600

    Update view-hospitals.php

--- view-hospitals.php
+++ view-hospitals.php
@@ -41,7 +41,7 @@ while ($hospital = $hospitals->fetch_assoc()) {
     </td>
     <td>
         <form method="post" action="">
-        <input type="hidden" name="cid" value="<?php echo $hospital ['hospital_id'];?>">
+        <input type="hidden" name="hid" value="<?php echo $hospital ['hospital_id'];?>">
           <input type="hidden" name="actionType" value="Delete">
             <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
