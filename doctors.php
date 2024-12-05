1ab1b84f68ac5b41824e8bcc259decc897914161
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-05 00:30:19 -0600

    Update doctors.php

--- doctors.php
+++ doctors.php
@@ -1,9 +1,40 @@
+
 <?php
 require_once("util-db.php");
 require_once("model-doctors.php");
 
 $pageTitle = "Doctors";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add": 
+      if (insertDoctor($_POST['dName'],$_POST['dSpecialization'])) {
+      echo '<div class="alert alert-success" role="alert"> A doctor has been added!</div>';
+      } else {
+      echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
+      }
+      break;
+
+     case "Edit": 
+      if (updateDoctor($_POST['dName'],$_POST['dSpecialization'],$_POST['did'])) {
+      echo '<div class="alert alert-success" role="alert"> A doctor has been edited!</div>';
+      } else {
+      echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
+      }
+      break;
+    
+      case "Delete": 
+      if (deleteDoctor($_POST['did'])) {
+      echo '<div class="alert alert-success" role="alert"> A doctor has been deleted!</div>';
+      } else {
+      echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
+      }
+      break;
+   
+  }
+}
+
 $doctors=selectDoctors();
 include "view-doctors.php";
 include "view-footer.php";
