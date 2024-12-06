daab5d5a7ec5fc8860d226de6e7fbffc2f13c86b
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-05 18:51:36 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -5,33 +5,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+      .navbar {
+        background-color: #000080 !important; /* Navy blue color for the navbar */
+      }
+      .navbar-brand img {
+        height: 40px; /* Adjust the height of the logo */
+        margin-right: 10px;
+      }
+      .navbar .nav-link {
+        color: white !important; /* White text for nav links */
+      }
+      .navbar .nav-link:hover {
+        color: #ffcccb !important; /* Light pink for hover effect */
+      }
+    </style>
   </head>
   <body>
     <div class="container">
-      <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="doctors.php">Doctors</a>
-        </li>
-         <li class="nav-item">
-          <a class="nav-link" href="clients.php">Clients</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="hospitals.php">Hospitals</a>
-        </li>
-         <li class="nav-item">
-          <a class="nav-link" href="doctors-with-clients.php">Doctors with clients</a>
-        </li>
-      </ul>
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="https://thumbs.dreamstime.com/b/red-cross-vector-icon-hospital-sign-medical-health-first-aid-symbol-isolated-vhite-modern-gradient-design-141217893.jpg" alt="Hospital Logo">
+            Navbar
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="doctors.php">Doctors</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="clients.php">Clients</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="hospitals.php">Hospitals</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="doctors-with-clients.php">Doctors with clients</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+AMvyZqU1zL0vb6+AMfum13BjuGlk" crossorigin="anonymous"></script>
+  </body>
+</html>
