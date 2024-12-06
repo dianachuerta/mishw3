fa857bea53278c8ae4aa94294b0d8909bf6ca142
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-12-05 19:14:26 -0600

    Update index.php

--- index.php
+++ index.php
@@ -56,4 +56,4 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+AMvyZqU1zL0vb6+AMfum13BjuGlk" crossorigin="anonymous"></script>
   </body>
-</html>
+</html> 
